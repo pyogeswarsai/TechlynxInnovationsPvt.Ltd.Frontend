cf0117a2e0a40411a8b4a800c8eda031cb66f084
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-08-24 09:19:31 +0530

    contact management updated

--- src/components/ManagementInfo.jsx
+++ src/components/ManagementInfo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../components/Contact.css'
+import HR1 from '../assets/HR1.jpg'
+import HR2 from '../assets/HR2.jpg'
 
 const ManagementInfo = () => {
   return (
@@ -13,8 +15,8 @@ const ManagementInfo = () => {
           {/* HR 1 */}
           <div className="col-md-4 mt-2">
             <div className="card p-3 shadow">
-              <img src="src\assets\HR1.jpg" height={"320px"}  alt="" />
-              <h4 className="mt-2">Renuka </h4>
+              <img src={HR1} height={"320px"}  alt="" />
+              <h4 className="mt-2">Renuka Kolasanakoti</h4>
               <p className="text-body">
                 <strong>Designation:</strong> Senior HR Executive
               </p>
@@ -30,7 +32,7 @@ const ManagementInfo = () => {
           {/* HR 2 */}
           <div className="col-md-4 mt-2">
             <div className="card p-3 shadow">
-                <img src="src\assets\HR2.jpg" alt="" />
+                <img src={HR2} height={"320px"} alt="" />
               <h4 className="mt-2">Vignasai Chunduri</h4>
               <p className="text-body">
                 <strong>Designation:</strong> Junior HR Manager
